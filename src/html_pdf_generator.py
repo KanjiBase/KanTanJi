850319a011c0f7aac2a47feded3b2c585cad2e64
Aiosa <469130@mail.muni.cz>
2025-11-04 22:35:51 +0100

    chore: prevent pdf to output html template

--- src/html_pdf_generator.py
+++ src/html_pdf_generator.py
@@ -200,8 +200,8 @@ def generate(name, data, radicals, path_getter, is_debug_run):
         pdfkit.from_string(content_html, pdf_output_path, options=options)
 
         # Could also output html
-        with open(f"{path_getter(name)}/{name}.html", "w", encoding="UTF-8") as file:
-            file.write(content_html)
+        # with open(f"{path_getter(name)}/{name}.html", "w", encoding="UTF-8") as file:
+        #     file.write(content_html)
 
     return True
 
